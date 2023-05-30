74d1005f527ccf5562157eb8e4c7965964ad4519
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-05-30 10:12:15 +0530

    Delete script.js

--- script/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-    var navbar = document.getElementById("navbar");
-    if (window.scrollY > 80) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
-
